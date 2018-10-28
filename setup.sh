82b6bf5508a8dcaec62fea39c259074fab833312
Damoon <damoon.rashidi@gmail.com>
2018-10-28 21:08:03 +0100

    create init file

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#install oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+#Install bat and use it over cat
+brew install bat
+
+#Install diff-so-fancy for better git diffs
+brew install diff-so-fancy
+
+#install fzf and use it for ctrl+r
+brew install fzf
+$(brew --prefix)/opt/fzf/install
+
+#install zsh autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+
